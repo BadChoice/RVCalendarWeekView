8232284d3635c0d32dfa4ffe5becdb65e01ed52f
Jordi Puigdellívol <jordi@gloobus.net>
2016-09-02 09:39:29 +0200

    starting with pinchable

---
+++ RVCalendarWeekView/Lib/WeekView/Decorators/MSWeekViewDecoratorPinchable.h
@@ -0,0 +1,13 @@
+//
+//  MSWeekViewDecoratorPinchable.h
+//  RVCalendarWeekView
+//
+//  Created by Jordi Puigdellívol on 2/9/16.
+//  Copyright © 2016 revo. All rights reserved.
+//
+
+#import "MSWeekViewDecorator.h"
+
+@interface MSWeekViewDecoratorPinchable : MSWeekViewDecorator
+
+@end
---
+++ RVCalendarWeekView/Lib/WeekView/Decorators/MSWeekViewDecoratorPinchable.m
@@ -0,0 +1,21 @@
+//
+//  MSWeekViewDecoratorPinchable.m
+//  RVCalendarWeekView
+//
+//  Created by Jordi Puigdellívol on 2/9/16.
+//  Copyright © 2016 revo. All rights reserved.
+//
+
+#import "MSWeekViewDecoratorPinchable.h"
+
+@implementation MSWeekViewDecoratorPinchable
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
